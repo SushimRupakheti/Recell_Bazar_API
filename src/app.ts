b045c2afb002976de8d9fc7a7da44fb60831a321
SushimRupakheti <sushimrupakheti120@gmail.com>
2026-02-23 22:37:35 +0545

    admin payment done

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { PORT } from './config';
 import authRoutes from './routes/auth.route';
 import adminUserRoute from './routes/admin/user.route';
 import adminItemRoute from './routes/admin/item.route';
+import adminPaymentRoute from './routes/admin/payment.route';
 import itemRoutes from './routes/item.route';
 import paymentRoutes from './routes/payment.route';
 import paymentController from './controllers/payment.controller';
@@ -50,6 +51,7 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/admin/users', adminUserRoute);
 app.use('/api/admin/items', adminItemRoute);
+app.use('/api/admin/payments', adminPaymentRoute);
 app.use("/uploads", express.static("uploads"));
 
 app.use("/api/items", itemRoutes);
